cd87b5b4346599c49f22d783543c4543c89e10f3
jesusrmoreno <jesus.moreno@experoinc.com>
2018-10-04 13:42:47 -0500

    Comment property value and move populate logic to property method

--- main.go
+++ main.go
@@ -8,13 +8,10 @@ import (
 	"log"
 	"math/rand"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
-	"github.com/icrowley/fake"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -28,187 +25,6 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
-func populate(prop ArrayOrString) interface{} {
-	if len(prop.Values) > 0 {
-		return prop.Values[random(0, len(prop.Values))]
-	}
-	p := strings.Title(prop.Value)
-	switch p {
-	case "Name":
-		v := []string{fake.MaleFullName(), fake.FemaleFullName()}
-		return v[random(0, 2)]
-	case "Brand":
-		return fake.Brand()
-	case "Character":
-		return fake.Character()
-	case "Characters":
-		return fake.Characters()
-	case "City":
-		return fake.City()
-	case "Color":
-		return fake.Color()
-	case "Company":
-		return fake.Company()
-	case "Continent":
-		return fake.Continent()
-	case "Country":
-		return fake.Country()
-	case "CreditCardNum":
-		return fmt.Sprintf("0000-%d-%d-%d", random(1000, 10000), random(1000, 10000), random(1000, 10000))
-	case "CreditCardType":
-		return fake.CreditCardType()
-	case "Currency":
-		return fake.Currency()
-	case "CurrencyCode":
-		return fake.CurrencyCode()
-	case "Day":
-		return fake.Day()
-	case "Digits":
-		return fake.Digits()
-	case "DomainName":
-		return fake.DomainName()
-	case "DomainZone":
-		return fake.DomainZone()
-	case "EmailAddress":
-		return fake.EmailAddress()
-	case "EmailBody":
-		return fake.EmailBody()
-	case "EmailSubject":
-		return fake.EmailSubject()
-	case "FemaleFirstName":
-		return fake.FemaleFirstName()
-	case "FemaleFullName":
-		return fake.FemaleFullName()
-	case "FemaleFullNameWithPrefix":
-		return fake.FemaleFullNameWithPrefix()
-	case "FemaleFullNameWithSuffix":
-		return fake.FemaleFullNameWithSuffix()
-	case "FemaleLastName":
-		return fake.FemaleLastName()
-	case "FemalePatronymic":
-		return fake.FemalePatronymic()
-	case "FirstName":
-		return fake.FirstName()
-	case "FullName":
-		return fake.FullName()
-	case "FullNameWithPrefix":
-		return fake.FullNameWithPrefix()
-	case "FullNameWithSuffix":
-		return fake.FullNameWithSuffix()
-	case "Gender":
-		return fake.Gender()
-	case "GenderAbbrev":
-		return fake.GenderAbbrev()
-	case "GetLangs":
-		return fake.GetLangs()
-	case "HexColor":
-		return fake.HexColor()
-	case "HexColorShort":
-		return fake.HexColorShort()
-	case "IPv4":
-		return fake.IPv4()
-	case "IPv6":
-		return fake.IPv6()
-	case "Industry":
-		return fake.Industry()
-	case "JobTitle":
-		return fake.JobTitle()
-	case "Language":
-		return fake.Language()
-	case "LastName":
-		return fake.LastName()
-	case "Latitude":
-		return fake.Latitude()
-	case "LatitudeDegrees":
-		return fake.LatitudeDegrees()
-	case "LatitudeDirection":
-		return fake.LatitudeDirection()
-	case "LatitudeMinutes":
-		return fake.LatitudeMinutes()
-	case "LatitudeSeconds":
-		return fake.LatitudeSeconds()
-	case "Longitude":
-		return fake.Longitude()
-	case "LongitudeDegrees":
-		return fake.LongitudeDegrees()
-	case "LongitudeDirection":
-		return fake.LongitudeDirection()
-	case "LongitudeMinutes":
-		return fake.LongitudeMinutes()
-	case "LongitudeSeconds":
-		return fake.LongitudeSeconds()
-	case "MaleFirstName":
-		return fake.MaleFirstName()
-	case "MaleFullName":
-		return fake.MaleFullName()
-	case "MaleFullNameWithPrefix":
-		return fake.MaleFullNameWithPrefix()
-	case "MaleFullNameWithSuffix":
-		return fake.MaleFullNameWithSuffix()
-	case "MaleLastName":
-		return fake.MaleLastName()
-	case "MalePatronymic":
-		return fake.MalePatronymic()
-	case "Model":
-		return fake.Model()
-	case "Month":
-		return fake.Month()
-	case "MonthNum":
-		return fake.MonthNum()
-	case "MonthShort":
-		return fake.MonthShort()
-	case "Paragraph":
-		return fake.Paragraph()
-	case "Paragraphs":
-		return fake.Paragraphs()
-	case "Patronymic":
-		return fake.Patronymic()
-	case "Phone":
-		return fake.Phone()
-	case "Product":
-		return fake.Product()
-	case "ProductName":
-		return fake.ProductName()
-	case "Sentence":
-		return fake.Sentence()
-	case "Sentences":
-		return fake.Sentences()
-	case "SimplePassword":
-		return fake.SimplePassword()
-	case "State":
-		return fake.State()
-	case "StateAbbrev":
-		return fake.StateAbbrev()
-	case "Street":
-		return fake.Street()
-	case "StreetAddress":
-		return fake.StreetAddress()
-	case "Title":
-		return fake.Title()
-	case "TopLevelDomain":
-		return fake.TopLevelDomain()
-	case "UserAgent":
-		return fake.UserAgent()
-	case "UserName":
-		return fake.UserName()
-	case "WeekDay":
-		return fake.WeekDay()
-	case "WeekDayShort":
-		return fake.WeekDayShort()
-	case "WeekdayNum":
-		return fake.WeekdayNum()
-	case "Word":
-		return fake.Word()
-	case "Words":
-		return fake.Words()
-	case "Year":
-		return fake.Year(0, time.Now().Year())
-	case "Zip":
-		return fake.Zip()
-	}
-	return prop
-}
-
 // GraphDefinition holds the initial and final graph structure
 type GraphDefinition struct {
 	NodeDefs            []NodeDefinition         `json:"nodes"`
@@ -217,7 +33,7 @@ type GraphDefinition struct {
 }
 
 // PropertiesDefinition holds the available properties on either a node or an edge
-type PropertiesDefinition map[string]ArrayOrString
+type PropertiesDefinition map[string]Property
 
 // NodeDefinition holds a node object
 type NodeDefinition struct {
@@ -262,8 +78,8 @@ type Graph struct {
 
 func generateProperties(p PropertiesDefinition) map[string]interface{} {
 	props := map[string]interface{}{}
-	for prop, kind := range p {
-		props[prop] = populate(kind)
+	for propertyKey, property := range p {
+		props[propertyKey] = property.Populate()
 	}
 	return props
 }
